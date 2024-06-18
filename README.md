26bf87dfc7843a2914a0968fb8bc82dc6a430a6e
teliang <telianghung@outlook.com>
2024-06-18 14:41:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Spotifyの歌詞の背景色を変更するスクリプト
+
+[[Other] Toggle on/off the dynamic background color of the lyrics feature.](https://community.spotify.com/t5/Live-Ideas/Other-Toggle-on-off-the-dynamic-background-color-of-the-lyrics/idi-p/5256331)
+
+多くの人々がSpotifyの動的な背景色を好まないかもしれません。このスクリプトを使用すると、自分の好きな背景色に変更することができます。
+
+## 機能の概要
+
+このスクリプトは、Spotifyの歌詞表示時に動的に変わる背景色を固定の色に変更することができます。これにより、視覚的な一貫性を保つことができます。
+
+## 使用前と使用後
+
+**使用前:**
+![使用前](https://github.com/Teliang/change-background-color-of-spotify-lyrics/raw/main/images/Screenshot1.png)
+
+**使用後:**
+![使用後](https://github.com/Teliang/change-background-color-of-spotify-lyrics/raw/main/images/Screenshot2.png)
+
+## インストール方法
+
+1. [Tampermonkey](https://www.tampermonkey.net/) をインストールします。
+2. このスクリプトをTampermonkeyに追加します。
+
+## カスタマイズ
+
+好きな背景色に変更するために、以下のコードを編集してください：
+
+```javascript
+const color = 'rgb(121, 121, 121)'; // 希望する色に変更
+```
---
+++ src/main.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Change the background color of Spotify lyrics.
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-14
+// @description  Change the background color of Spotify lyrics.
+// @author       telianghung@outlook.com
+// @match        https://open.spotify.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=spotify.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const fun = () =>{
+         // 希望なcolor
+        const color = 'rgb(121, 121, 121)'
+
+        if(location.href !== "https://open.spotify.com/lyrics") return;
+
+        const backgroundColor = `--lyrics-color-background: ${color};`
+
+        const lyricsElement = document.querySelector('[aria-label="Spotify"] > :first-child')
+
+        if (!lyricsElement) return;
+
+        // 正規表現を使って --lyrics-color-background: rgb(...) の値を抽出
+        const match = lyricsElement.style.cssText.match(/--lyrics-color-background:\s*rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\);/);
+
+        // 抽出した値が期待する値かどうかを確認
+        if (match && match[0] !== backgroundColor) {
+            lyricsElement.style.cssText= lyricsElement.style.cssText.replace(/(--lyrics-color-background:\s*rgb\([^)]+\);)/g, backgroundColor)
+            console.log('background colorを変えた')
+        }
+    }
+
+
+    setInterval(fun, 1000)
+})();
